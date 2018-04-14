d36f62ad46030faea95db89627b0c1eef8e88d23
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2018-04-14 12:24:32 -0500

    Fetch data from store.

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -21,6 +21,23 @@ class ViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
     fileprivate func save(name: String) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
